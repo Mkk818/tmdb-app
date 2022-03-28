42faf48edf77700afff3693ec148ebe9f58983f4
Mikiko Noro <miriks818@gmail.com>
2022-03-28 22:30:15 +0900

    再deploy

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
-import { request } from "https";
+// import { request } from "https";
 import React, { useState, useEffect } from "react";
-import { DEFAULT_MIN_VERSION } from "tls";
+// import { DEFAULT_MIN_VERSION } from "tls";
 import axios from "../axios";
 import { requests } from "../request";
 import "./Banner.scss";
